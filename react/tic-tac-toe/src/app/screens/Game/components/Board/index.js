5d9cdb2221b695807c81c6b1de8ccbff5712bac1
bhflm <germanbonin@gmail.com>
2019-06-25 14:50:17 -0300

    (add): basic tictactoe flow

--- react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
@@ -17,16 +17,49 @@ class Board extends Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (this.calculateWinner(squares) || squares[i]) return;
+
     squares[i] = this.state.player;
-    this.setState({squares : squares, player : this.changeTurn() });
+    this.setState({squares : squares, player: this.changeTurn() });
   }
 
   renderSquare(i) {
     return <Square value={this.state.squares[i]} onClick={ ()=> this.handleClick(i)} />;
   }
 
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   render() {
-    const status = this.state.player + `'s turn`;
+    const winner = this.calculateWinner(this.state.squares);
+    let status = this.state.player + `'s turn`;
+
+    winner ? status = winner : status = this.state.player + `'s turn`;
+
+    if (winner) {
+      status = 'Winner ' + winner;
+    }
+    else {
+
+    }
+
     return (
       <div>
         <div className={styles.status}>{status}</div>
