a8aad5ecba05c8288b75fcae49339ab0f2b68297
bhflm <germanbonin@gmail.com>
2019-06-25 11:11:28 -0300

    (add): pass prop to Square component

--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
 
-const Square = () => (
+const Square = ({ value }) => (
   <button type="button" className={styles.square}>
-    {/* TODO */}
+    { value }
   </button>
 );
 
