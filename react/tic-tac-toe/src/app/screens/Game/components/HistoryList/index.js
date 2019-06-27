a299608457c22fefd395474642248942dd457be5
bhflm <germanbonin@gmail.com>
2019-06-27 13:54:07 -0300

    (fix): lint issues within HistoryList

--- react/tic-tac-toe/src/app/screens/Game/components/HistoryList/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/HistoryList/index.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class HistoryList extends Component {
-  render() {
-    return (
-      <ol>
-        {this.props.history.map((step, move) => {
-          const desc = move ? `Back to #${move}` : 'Back to start';
-          return (
-            <li key={move}>
-              <button onClick={() => this.props.onChangeHistory(move)}>{desc}</button>
-            </li>
-          );
-        })}
-      </ol>
-    );
-  }
+function HistoryList({ onChangeHistory, history }) {
+  return (
+    <ol>
+      {history.map((_step, move) => {
+        const desc = move ? `Back to #${move}` : 'Back to start';
+        return (
+          // index is the only way to identify each square and quare position won’t change
+          // eslint-disable-next-line react/no-array-index-key
+          <li key={move}>
+            <button type="button" onClick={onChangeHistory(move)}>
+              {desc}
+            </button>
+          </li>
+        );
+      })}
+    </ol>
+  );
 }
+
+HistoryList.propTypes = {
+  history: PropTypes.arrayOf(PropTypes.array),
+  onChangeHistory: PropTypes.func
+};
 
 export default HistoryList;
--- react/tic-tac-toe/src/app/screens/Game/constants.js
+++ react/tic-tac-toe/src/app/screens/Game/constants.js
@@ -8,3 +8,5 @@ exports.boardLines = [
   [0, 4, 8],
   [2, 4, 6]
 ];
+
+exports.boardRows = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
