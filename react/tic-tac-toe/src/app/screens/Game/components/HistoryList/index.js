5b613192768da2bd98c8f26dc688e990ccb25b40
bhflm <germanbonin@gmail.com>
2019-06-27 15:23:27 -0300

    (refactor): lint issues within historyList

--- react/tic-tac-toe/src/app/screens/Game/components/HistoryList/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/HistoryList/index.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function HistoryList({ onChangeHistory, history }) {
-  return (
-    <ol>
-      {history.map((_step, move) => {
-        const desc = move ? `Back to #${move}` : 'Back to start';
-        return (
-          // index is the only way to identify each square and quare position won’t change
-          // eslint-disable-next-line react/no-array-index-key
-          <li key={move}>
-            <button type="button" onClick={onChangeHistory(move)}>
-              {desc}
-            </button>
-          </li>
-        );
-      })}
-    </ol>
-  );
+class HistoryList extends Component {
+  handleOnClick = () => {
+    const { move, onChangeHistory } = this.props;
+    onChangeHistory(move);
+  }
+
+  render () {
+    return (
+      <ol>
+        {history.map((_step, move) => {
+          const desc = move ? `Back to #${move}` : 'Back to start';
+          return (
+            // index is the only way to identify each square and quare position won’t change
+            // eslint-disable-next-line react/no-array-index-key
+            <li key={move}>
+              <button type="button" onClick={this.handleOnClick}>
+                {desc}
+              </button>
+            </li>
+          );
+        })}
+      </ol>
+    );
+  }
 }
 
 HistoryList.propTypes = {
-  history: PropTypes.arrayOf(PropTypes.array),
+  move: PropTypes.number,
   onChangeHistory: PropTypes.func
 };
 
