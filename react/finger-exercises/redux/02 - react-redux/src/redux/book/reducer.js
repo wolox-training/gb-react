e0fc4e79d141f48e36a6de4da9f2f7f3ddb0b0ab
bhflm <germanbonin@gmail.com>
2019-07-01 17:59:49 -0300

    (add): logic for redux actions

--- react/finger-exercises/redux/02 - react-redux/src/redux/book/reducer.js
+++ react/finger-exercises/redux/02 - react-redux/src/redux/book/reducer.js
@@ -9,15 +9,15 @@ const initialState = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     case actions.GET_BOOKS: // TODO to implement the logic
-      return { ...state };
+      return { ...state, books: [...state.books] };
     case actions.ADD_TO_CART: // TODO to implement the logic
-      return { ...state };
+      return { ...state, bookSelected: [...state.bookSelected, actions.payload.itemId]};
     case actions.ADD_ITEM: // TODO to implement the logic
-      return { ...state };
+      return { ...state, books: [ ...state.books, actions.payload.itemId] };
     case actions.REMOVE_ITEM: // TODO to implement the logic
-      return { ...state };
+      return { ...state, books: [...state.books.filter(each => each.id !== actions.payload.itemId)] };
     case actions.SEARCH_ITEM: // TODO to implement the logic
-      return { ...state };
+      return { ...state, item: [...state.books.filter(each => each.id === actions.payload.itemId)]};
     default:
       return state;
   }
