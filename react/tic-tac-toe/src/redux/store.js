16c9b8bf00afe17d62330f59ad4f51a6a0b2d034
bhflm <germanbonin@gmail.com>
2019-07-05 12:15:02 -0300

    (add): redux basic store

--- react/tic-tac-toe/src/redux/store.js
+++ react/tic-tac-toe/src/redux/store.js
@@ -1,9 +1,10 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { createStore, compose } from 'redux';
 
 import reducer from './tictactoe/reducer';
 
-export default createStore(
-  reducer,
-  devToolsEnhancer()
-);
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const rootReducer = { ...reducer };
+const store = createStore(rootReducer, composeEnhancers());
+
+export default store;
