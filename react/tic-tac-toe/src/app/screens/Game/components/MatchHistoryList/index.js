c4107a27bc4a33ed6d3b6e11728325e738864cb4
bhflm <germanbonin@gmail.com>
2019-07-05 19:40:18 -0300

    (fix): request service working

--- react/tic-tac-toe/src/app/screens/Game/components/MatchHistoryList/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/MatchHistoryList/index.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { get } from 'lodash';
 
-import matchesService from '../../../../../services/matchesService';
-import actionsCreator from '../../../../../redux/tictactoe/actions';
+import MatchesService from '../../../../../services/MatchesService';
+import actionsCreators from '../../../../../redux/tictactoe/actions';
 
 import MatchHistoryEntry from './components/MatchHistoryEntry';
 
 class MatchHistoryList extends Component {
-  async componentDidMount(dispatch) {
-    const response = await matchesService.getHistoric();
-    console.warn('response: ', response);
-    const matches = get(response);
-    if (response.ok) {
-      dispatch(actionsCreator.getHistoric(matches));
-    } else {
-      dispatch(actionsCreator.setUsersError('germo no sabes codear'));
-    }
+  componentDidMount() {
+    this.props.getHistoric();
   }
 
   render() {
@@ -29,6 +21,7 @@ class MatchHistoryList extends Component {
 }
 
 MatchHistoryList.propTypes = {
+  getHistoric: PropTypes.func.isRequired,
   historicEntries: PropTypes.isRequired
 };
 
@@ -37,7 +30,13 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  getHistoric: () => dispatch(actionsCreator.getHistoric(dispatch))
+  getHistoric: async () => {
+    const response = await MatchesService.getHistoric();
+    if (response.ok) {
+      dispatch(actionsCreators.getHistoric(response.data));
+    }
+  }
 });
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(MatchHistoryList);
