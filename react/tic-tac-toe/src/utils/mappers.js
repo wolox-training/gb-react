74d3993cb7e238a328312d6bcb3140d090da4600
bhflm <germanbonin@gmail.com>
2019-07-10 16:25:24 -0300

    (fix): render match history with correct style

--- react/tic-tac-toe/src/utils/mappers.js
+++ react/tic-tac-toe/src/utils/mappers.js
@@ -1,6 +1,17 @@
-exports.parseMatchesResponse = raw => ({
-  firstPlayer: raw.player_one,
-  secondPlayer: raw.player_two,
-  winner: raw.winner,
-  date: raw.createdAt
-});
+const getWinner = matchResult => {
+  if (matchResult.winner === 'tie') {
+    return matchResult.winner;
+  }
+  return matchResult.winner === 'player_one' ? matchResult.player_one : matchResult.player_two;
+};
+
+exports.parseMatchesResponse = raw => {
+  const matchWinner = getWinner(raw);
+  console.log('MATCH WINNER: ', matchWinner);
+  return {
+    firstPlayer: raw.player_one,
+    secondPlayer: raw.player_two,
+    winner: matchWinner,
+    date: raw.createdAt
+  };
+};
