7ad470e2ffb11baed58260553a6e273716744a3c
bhflm <germanbonin@gmail.com>
2019-07-17 14:26:13 -0300

    (refactor): match entry styles

--- react/tic-tac-toe/src/app/screens/Game/components/MatchHistoryList/components/MatchHistoryEntry/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/MatchHistoryList/components/MatchHistoryEntry/index.js
@@ -6,13 +6,13 @@ import styles from './styles.module.scss';
 function MatchHistoryEntry({ matchEntry }) {
   return (
     <div className={styles.matchEntryContainer}>
-      <span className={styles.matchEntry}>
+      <span className={`${styles.row} ${styles.matchEntry}`}>
         {matchEntry && matchEntry.firstPlayer}
       </span>
-      <span className={styles.matchEntry}>
+      <span className={`${styles.row} ${styles.matchEntry}`}>
         {matchEntry && matchEntry.secondPlayer}
       </span>
-      <span className={styles.matchEntry}>
+      <span className={`${styles.row} ${styles.matchEntry}`}>
         {matchEntry && matchEntry.winner}
       </span>
     </div>
