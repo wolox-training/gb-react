55f6f90a429482b7ff0884debe51bd41a553f9db
bhflm <germanbonin@gmail.com>
2019-07-17 14:36:25 -0300

    (refactor): export const in validations

--- react/tic-tac-toe/src/app/screens/Login/Validations/index.js
+++ react/tic-tac-toe/src/app/screens/Login/Validations/index.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line no-useless-escape
 const VALID_EMAIL = /^\w+([\.-]?\w+)*@wolox.com/;
 
-exports.isRequired = value => value ? undefined : 'Required field';
+export const isRequired = value => value ? undefined : 'Required field';
 
-exports.hasLength = value =>
+export const hasLength = value =>
   value.length < 8 ? 'Value length has to be greater than 8 characters' : undefined;
 
-exports.isValidEmail = value =>
+export const isValidEmail = value =>
   VALID_EMAIL.test(value) ? undefined : 'Domain needs to be @wolox.com';
