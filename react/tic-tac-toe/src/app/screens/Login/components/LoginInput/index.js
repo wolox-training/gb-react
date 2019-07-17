0b6cdf356510dbbf1a9106dcdaad59edc9d593c6
bhflm <germanbonin@gmail.com>
2019-07-17 14:53:28 -0300

    (remove): duplicated label

--- react/tic-tac-toe/src/app/screens/Login/components/LoginInput/index.js
+++ react/tic-tac-toe/src/app/screens/Login/components/LoginInput/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import styles from './styles.module.scss';
 
-function LoginInput({ input, label, type, meta: { error, submitFailed } }) {
+function LoginInput({ input, type, meta: { error, submitFailed } }) {
   return (
     <div>
-      <label>{label}</label>
       <input
         {...input}
-        placeholder={label}
         type={type}
         className={styles.loginInput}
       />
