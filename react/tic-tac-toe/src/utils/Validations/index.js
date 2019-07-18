ec2889eae9423522d450da65411e062af583ea36
bhflm <germanbonin@gmail.com>
2019-07-18 19:21:49 -0300

    (refactor): replaced undef for null in validations

--- react/tic-tac-toe/src/utils/Validations/index.js
+++ react/tic-tac-toe/src/utils/Validations/index.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-useless-escape
 const VALID_EMAIL = /^\w+([\.-]?\w+)*@wolox.com/;
 
-const isRequired = value => (value ? undefined : 'Required field');
+const isRequired = value => (value ? null : 'Required field');
 
 const hasLength = value =>
-  value.length < 8 ? 'Value length has to be greater than 8 characters' : undefined;
+  value && value.length < 8 ? 'Value length has to be greater than 8 characters' : null;
 
-const isValidEmail = value => (VALID_EMAIL.test(value) ? undefined : 'Domain needs to be @wolox.com');
+const isValidEmail = value => (VALID_EMAIL.test(value) ? null : 'Domain needs to be @wolox.com');
 
 export const validateEmail = [isRequired, isValidEmail];
 
