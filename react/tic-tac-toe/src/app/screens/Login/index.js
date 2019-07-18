ee026f3cac41512183ad1c85f49a4cca2a0a362c
bhflm <germanbonin@gmail.com>
2019-07-18 14:48:39 -0300

    (refactor): login handles token through token

--- react/tic-tac-toe/src/app/screens/Login/index.js
+++ react/tic-tac-toe/src/app/screens/Login/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -8,33 +7,19 @@ import actionsCreators from '../../../redux/users/actions';
 import Login from './layout';
 
 class LoginContainer extends Component {
-  handleSubmit = submitted => {
-    this.props.login({
-      email: submitted.email,
-      password: submitted.password
-    });
-  };
+  handleSubmit = ({ email, password }) => this.props.login({ email, password });
 
   render() {
-    const { isLoggedIn } = this.props;
-    if (isLoggedIn) {
-      return <Redirect to="/game" />;
-    }
-    return <Login onSubmit={this.handleSubmit} />;
+    return (<Login onSubmit={this.handleSubmit} />);
   }
 }
 
 LoginContainer.propTypes = {
-  login: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.string
+  login: PropTypes.func.isRequired
 };
-
-const mapStateToProps = state => ({
-  isLoggedIn: state.usersReducer.token
-});
 
 const mapDispatchToProps = dispatch => ({
   login: userData => dispatch(actionsCreators.login(userData))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer);
+export default connect(null, mapDispatchToProps)(LoginContainer);
